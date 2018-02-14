9d6c1151950626f9f75ff7c0d5cfbc28b1d69208
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-02-14 17:17:33 +0100

    fix data transformation

--- redux/reducers/fetched.js
+++ redux/reducers/fetched.js
@@ -4,10 +4,11 @@ const participantsFetched = (state = [], action) => {
   switch (action.type) {
     case Types.PARTICIPANTS_FETCHED:
 
-      return action.payload.map((item) => {
-          return {[item.code] : item};
+      let newData = {};
+      action.payload.forEach(function(item){
+        newData[[item.code]] = item;
       });
-
+      return newData;
       //return Object.assign({}, state, action.payload);
     default:
       return state
